4a87d3657179dfca29d34012b311ddeb6cf7a678
tillyninjaspace <squealingpet@gmail.com>
2021-07-27 17:56:30 -0700

    postData does not need to be json

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -13,19 +13,42 @@ export const fetchPosts= () => dispatch => {
 export const createPost = (postData) => dispatch => {
     
     console.log("postAction.js is working")
-    
-    fetch('https://jsonplaceholder.typicode.com/posts',
-    {method: 'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify(postData)
+//ORIGINAL
+    // fetch('https://jsonplaceholder.typicode.com/posts',
+    // {method: 'POST',
+    // headers: {'content-type': 'application/json'},
+    // body: JSON.stringify(postData)
+    // })
+    // .then(res=> res.json())
+    // .then(post => 
+    //     dispatch({
+    //         type: NEW_POST,
+    //         payload: post
+    //     })
+    //     )
+//end ORIGINAL
+
+//lipstickstain_database call
+// let headers = new Headers();
+// headers.append('Content-Type', 'application/json');
+//     headers.append('Accept', 'application/json');
+//     headers.append('Origin','http://localhost:3000');
+
+fetch('https://lipstickstain.herokuapp.com/user/post',
+{method: 'POST',
+// headers: {'content-type': 'application/json'},
+// body: JSON.stringify(postData)
+body: postData
+})
+.then(res=> res.json())
+.then(post => 
+    dispatch({
+        type: NEW_POST,
+        payload: post
     })
-    .then(res=> res.json())
-    .then(post => 
-        dispatch({
-            type: NEW_POST,
-            payload: post
-        })
-        )
+    )
+//end of lipstickstain_database call
+
 }
 
 //dispatch is a resolver so it'll be type and payload
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -4,59 +4,184 @@ import { connect } from 'react-redux';
 import { createPost } from '../actions/postActions'
 
 
+
+//ORIGINAL POST FORM
+// class Postform extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             title: '',
+//             body: ''
+//         };
+//         this.onChange=this.onChange.bind(this);
+//         this.onSubmit = this.onSubmit.bind(this);
+//     }
+
+//     onChange(e) {
+//         this.setState({[e.target.name]: e.target.value});
+//     }
+
+//     onSubmit(e) {
+//         e.preventDefault();
+//         const post = {
+//             title: this.state.title,
+//             body: this.state.body
+//         }
+
+//         this.props.createPost(post);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Add Post</h1>
+//                 <form onSubmit={this.onSubmit}>
+//                     <div className="newPostForm">
+//                         <label>Title:</label>
+//                         <input type="text" name="title" value={this.state.title}
+//                             onChange={this.onChange}
+//                         />
+
+//                     </div>
+//                     <div className="newPostForm">
+//                         <label>Body:</label>
+//                         <textarea name="body" value={this.state.body}
+//                             onChange={this.onChange}
+//                         />
+                        
+//                     </div>
+//                     <button type="submit">Submit</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+//END of ORIGINAL POST FORM
+
+
+//REVERSIONING
+
+
 class Postform extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            title: '',
-            body: ''
+            name: '',
+            description: '',
+                location: '',
+                link: '',
+                picture: '',
+                selectedFile: '',
+                // fileUpload: '',
+                imagePreview: '',
         };
         this.onChange=this.onChange.bind(this);
+        this.onFileChange = this.onFileChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+            // fileImageUpload = React.useRef();
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
-    onSubmit(e) {
-        e.preventDefault();
-        const post = {
-            title: this.state.title,
-            body: this.state.body
-        }
+    onFileChange(e) {
+        this.setState({ selectedFile: e.target.files[0] });
+        this.setState({ imagePreview: URL.createObjectURL(e.target.files[0]) } );
+        this.setState({ picture: URL.createObjectURL(e.target.files[0]) });
+    };
 
-        this.props.createPost(post);
+    //EXTRA
+    // onImagePreview(e) {
+    //     this.setState({ imagePreview: URL.createObjectURL(e.target.files[0]) } )
+    // };
+
+    // onSetPicture(e) {
+    //     this.setState({ picture: URL.createObjectURL(e.target.files[0]) })
+    // };
+    //end EXTRA
+
+
+    async onSubmit (e) {
+        e.preventDefault();
+        const formData =  new FormData(); 
+            formData.append('name', this.state.name)
+            formData.append('description', this.state.description)
+            formData.append('location', this.state.location)
+            formData.append('link', this.state.link)
+            formData.append('picture', this.state.picture)
+            formData.append('productimage', this.state.selectedFile)         
+        console.log("What is form data? in onSubmit", formData)
+        await this.props.createPost(formData);
+  
+            this.setState({
+                name: '',
+                description: '',
+                imagePreview: '',
+              });
+        
     }
 
+    
     render() {
         return (
             <div>
                 <h1>Add Post</h1>
                 <form onSubmit={this.onSubmit}>
                     <div className="newPostForm">
-                        <label>Title:</label>
-                        <input type="text" name="title" value={this.state.title}
+                        <label>Name:</label>
+                        <input type="text" name="name" value={this.state.name}
                             onChange={this.onChange}
                         />
 
                     </div>
                     <div className="newPostForm">
-                        <label>Body:</label>
-                        <textarea name="body" value={this.state.body}
+                        <label>Description:</label>
+                        <textarea name="description" value={this.state.description}
                             onChange={this.onChange}
                         />
                         
                     </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        )
+
+                     <div className="newPostForm">
+                        <label>Location:</label>
+                        <input type="text" name="location" value={this.state.location}
+                            onChange={this.onChange}
+                        />
+
+                    </div>
+
+
+                     <div className="newPostForm">
+                        <label>Link:</label>
+                        <input type="text" name="link" value={this.state.link}
+                            onChange={this.onChange}
+                        />
+
+                    </div>
+
+                        <img alt="selected user's file" src={this.state.imagePreview} />
+                        <input type="file" label="Upload an image" 
+                        onChange={this.onFileChange} 
+                        required 
+                        // ref={fileImageUpload}
+                        />
+
+                <button type="submit">Submit</button>
+              </form>
+           </div>
+         )
     }
-}
+//END OF REVERSIONING          
+
+};
+
+
 
 Postform.propTypes = {
     createPost: PropTypes.func.isRequired
 };
+
+
 
 export default connect(null, {createPost})(Postform)

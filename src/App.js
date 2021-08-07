33f19044c1944f1805d01f71df0a4494f99bab9a
tillyninjaspace <squealingpet@gmail.com>
2021-08-06 22:11:49 -0700

    added footer and about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './sensorio_wine_bottles.jpg';
 import './App.css';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import About from './components/About';
+import Footer from './components/Footer';
 // import {Provider} from 'react-redux';
-import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect, Switch, NavLink} from 'react-router-dom';
 // import store from './store'
 import { SinglePostPage } from './components/SinglePage';
-
-
 
 function App() {
   // const history = useHistory();
@@ -19,10 +19,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <span style = {{color: "red"}}>Lipstick Stain</span> is under development.
+          <span>Lipstick Stain</span> is under development.
         </p>
         <p>Target Completion Date: August 31, 2021</p>
-        <a
+        <a style={{textAlign: "center"}}
           className="App-link"
           href="https://gracious-mcnulty-e733ac.netlify.app/portfolio.html"
           target="_blank"
@@ -32,26 +32,50 @@ function App() {
         </a>
       </header>
       {/* <Switch> */}
-      <Route
-            exact
-            path="/"
-            render={() => (
+      <NavLink to="/" className="nav">Home</NavLink>
+      <NavLink to="/about" className="nav">About Lipstick Stain</NavLink>
+      {/* <Route exact path="/"><Postform/><Posts /></Route> */}
+       <Route
+         exact
+      path="/"
+
+         render={() => (             
               <>
-                <Postform/>
+              <Postform/>
                 
-                <Posts />
-              </>
+               <Posts />
+              </>           
             )}
           />
+            
       
-     
-      <Route exact path="/posts/:postId" component = {SinglePostPage}/>
-      <Redirect to="/" />
+      <Route path="/about"><About/></Route>
+      <Route path="/posts/:postId" component = {SinglePostPage}/>
+      
       {/* </Switch> */}
+      {/* <Redirect to="/" /> */}
+      <Footer/>
     </div>
+   
     </BrowserRouter>
     // </Provider>
   );
 }
 
 export default App;
+
+
+// <Route
+//             exact
+//             path="/"
+
+//             render={() => (
+//               <>
+//                 <Postform/>
+                
+//                 <Posts />
+                
+//               </>
+//             )}
+
+//           />
---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div>
+            <h1>About Lipstick Stain</h1>
+            <p>After moving to San Luis Obispo County, I became more interested
+                in learning more about wine. With my husband, we'd go explore local wineries in
+                Edna Valley and Paso Robles, where many wineries are in San Luis Obispo County.
+                Some wineries would offer a unique ambiance like one that is castle, some that have 
+                acres and acres of vineyards for you to view as you're sipping on your wine. We've
+                also tasted wines in Arroyo Grande and Grover Beach. See our Wine Place section to 
+                see all of my personal reviews.
+            </p>
+            <p>
+                Leaving lipstick stains on their wine glasses, I'd take in the flavors and uniqueness
+                of each winery and vineyard and write my opinions here. 
+            </p>
+            <p>
+                Please drink responsibly and take an uber when you're feeling that it's unsafe to drive.
+            </p>
+            
+        </div>
+    )
+};
+
+export default About;
